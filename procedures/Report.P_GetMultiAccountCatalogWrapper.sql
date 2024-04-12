82731d4598e3c86959e7cc4970ac0613ee1458e5
Brandon Stahl <bstahl@tcetra.com>
2024-04-12 05:11:48 +0000

    Merged PR 8645: Multi Account Catalog Wrapper

---
+++ procedures/Report.P_GetMultiAccountCatalogWrapper.sql
@@ -0,0 +1,31 @@
+--=============================================
+--				:
+--	Author		: Brandon Stahl
+--				:
+--	Created		: 2024-04-11
+--				:
+--	Description	: Wraps P_GetMultiAccountCatalog to allow a parameter of comma separated
+--              : accounts for SSRS reporting
+--				:
+--	Test Data   : EXEC [Report].[P_GetMultiAccountCatalogWrapper] '13379'
+--=============================================
+CREATE OR ALTER PROCEDURE [Report].[P_GetMultiAccountCatalogWrapper]
+    (
+        @AccountIds VARCHAR(MAX)
+    )
+AS
+BEGIN
+    SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+
+    DECLARE @AccountIdList dbo.IDS;
+
+    INSERT INTO @AccountIdList
+    (
+        Id
+    )
+    SELECT ID
+    FROM dbo.fnSplitter(REPLACE(TRANSLATE(@AccountIDs, '	 ', '##'), '#', ''));
+
+    EXEC [Report].[P_GetMultiAccountCatalog] @AccountIdList;
+
+END;
